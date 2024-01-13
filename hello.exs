c0d579d1f19247dc7ed8a5a9aac09e1469ffae89
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-01-13 21:08:44 +0200

    update_case

--- hello.exs
+++ hello.exs
@@ -1,6 +1,14 @@
 # Define the Hello module with the world/1 function
-defmodule Hello do
-  def world(name) do
-    IO.puts("Hello #{name}")
-  end
+# defmodule Hello do
+#   def world(name) do
+#     IO.puts("Hello #{name}")
+#   end
+# end
+
+list = [1,2,3]
+case Enum.at(list,2) do
+
+  1 -> "this won't print"
+  3 -> "3 is matched"
+
 end
