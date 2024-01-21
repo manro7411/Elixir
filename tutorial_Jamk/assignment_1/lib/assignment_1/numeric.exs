49aa554858a0f121c96360a1fc1ed59080e7b743
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-01-21 22:50:21 +0200

    Part_2 is done

--- tutorial_Jamk/assignment_1/lib/assignment_1/numeric.exs
+++ tutorial_Jamk/assignment_1/lib/assignment_1/numeric.exs
@@ -4,7 +4,6 @@
 # Add code that asks a text line from the user (use IO.gets).
 # Add text <- You said that to the text that user entered.
 # Print the combined text into the console.
-
 # ---------------------------------------------------------------------------------
 
 variable = 123
@@ -13,3 +12,14 @@ IO.write("Input : ")
 input = IO.gets("")
 result = "The result is : #{String.trim(input)}"
 IO.puts result
+
+# Part 2
+# Write an Elixir script that calculates the result of 154 divided by 78 and prints it to the console.
+# Get the result of calculation (step 1) rounded to nearest integer and print it to console.
+# Get the result of calculation (step 1) and print only the integer part of it into the console.
+# ---------------------------------------------------------------------------------
+
+operation = 154/78
+IO.puts("The result : #{operation}")
+IO.puts("Rouded number : #{round(operation)}")
+IO.puts("Integer number : #{div(154,78)}")
