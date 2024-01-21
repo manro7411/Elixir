31054c082c717acc718322def6f111780ec7f83f
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-01-21 22:35:31 +0200

    modify assignment.ex

--- tutorial_Jamk/assignment_1/lib/assignment1.ex
+++ tutorial_Jamk/assignment_1/lib/assignment1.ex
@@ -1,18 +1,3 @@
 defmodule Assignment1 do
-  @moduledoc """
-  Documentation for `Assignment1`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Assignment1.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
