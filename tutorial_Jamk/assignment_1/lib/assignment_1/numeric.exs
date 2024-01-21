7b3ad6144e8db7fdee0f93aeaa98dc41458ff659
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-01-21 22:33:54 +0200

    Part_1 is done

--- tutorial_Jamk/assignment_1/lib/assignment_1/numeric.exs
+++ tutorial_Jamk/assignment_1/lib/assignment_1/numeric.exs
@@ -1,3 +1,15 @@
-defmodule  do
+# Part 1
+# Write an Elixir script that declares a variable and sets its value to 123.
+# Print the value of the variable to the console.
+# Add code that asks a text line from the user (use IO.gets).
+# Add text <- You said that to the text that user entered.
+# Print the combined text into the console.
 
-end
+# ---------------------------------------------------------------------------------
+
+variable = 123
+IO.puts "The numeric result : #{variable}"
+IO.write("Input : ")
+input = IO.gets("")
+result = "The result is : #{String.trim(input)}"
+IO.puts result
