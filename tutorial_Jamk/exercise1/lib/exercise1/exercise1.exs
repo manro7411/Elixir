5e50e9fbb0883fc1fae2103a3fd4857a916f9d57
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-01-22 15:04:51 +0200

    create directory && exercise1

---
+++ tutorial_Jamk/exercise1/lib/exercise1/exercise1.exs
@@ -0,0 +1,15 @@
+
+
+
+x = 42
+[^x,1,2,3] = [x,1,2,3]
+IO.puts("The result equals : #{x}")
+
+
+# defmodule  numeric do
+#   if x = 42 do
+#     IO.puts("The result equals :#{x}")
+#   end
+# end
+
+# IO.puts(numeric)
