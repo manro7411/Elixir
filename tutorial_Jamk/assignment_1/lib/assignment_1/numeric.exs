00a7fa2f6dbdf5b1595ba7c672aced431b2eabe9
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-01-24 23:23:51 +0200

    update part4_topic

--- tutorial_Jamk/assignment_1/lib/assignment_1/numeric.exs
+++ tutorial_Jamk/assignment_1/lib/assignment_1/numeric.exs
@@ -9,23 +9,23 @@ IO.puts result
 # Part 2
 operation = 154/78
 IO.puts("The result : #{operation}")
-IO.puts("Rounded number : #{round(operation)}")
+IO.puts("Rouded number : #{round(operation)}")
 IO.puts("Integer number : #{div(154,78)}")
 
-# Part 3
+#part3
 user = IO.gets("Enter the Sentences : ")
-alphabet = String.length(String.trim(user))
-IO.puts("Sentences : #{alphabet}")
-reverse_alphabet = String.reverse(Integer.to_string(alphabet))
-IO.puts("Reverse Sentence : #{reverse_alphabet}")
-modified = String.replace(user, "foo", "bar")
-IO.puts("Text is modified : #{modified}")
+alphabet =  String.length(String.trim(user))
+IO.puts("Sentences : #{alphabet}");
+reverse_alphabet = String.reverse(alphabet);
+IO.puts("Reverse Sentence #{reverse_alphabet}")
+modified = String.replace(user,"foo",'bar')
+IO.puts("Text is modified : {modified}")
 
-# Part 4
+#part4
 multiplication_function = fn a, b, c -> a * b * c end
 IO.puts "Enter the first number:"
-num1 = String.to_integer(String.trim(IO.gets("")))
+num1 = String.to_integer(IO.gets(""))
 IO.puts "Enter the second number:"
-num2 = String.to_integer(String.trim(IO.gets("")))
+num2 = String.to_integer(IO.gets(""))
 IO.puts "Enter the third number:"
-num3 = String.to_integer(String.trim(IO.gets("")))
+num3 = String.to_integer(IO.gets(""))
