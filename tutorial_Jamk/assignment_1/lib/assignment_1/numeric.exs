125a280b04f27bffcd8e9f4f85f9c2951c6a272f
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-01-28 11:15:58 +0200

    update input result function

--- tutorial_Jamk/assignment_1/lib/assignment_1/numeric.exs
+++ tutorial_Jamk/assignment_1/lib/assignment_1/numeric.exs
@@ -29,13 +29,14 @@ IO.puts("Enter the second number:")
 number2 = String.trim(IO.gets("")) |> String.to_integer()
 IO.puts("Enter the third number:")
 number3 = String.trim(IO.gets("")) |> String.to_integer()
-result_product = multiplication_function.(num1, num2, num3)
+result_product = multiplication_function.(number1,number2,number3)
 IO.puts "Product of the three numbers: #{result_product}"
-concat_function = fn list1, list2 -> list1 ++ list2 end
-list1 = [1, 2, 3]
-list2 = [4, 5, 6]
-result_concat = concat_function.(list1, list2)
-IO.inspect "Concatenated list: #{result_concat}"
-status_tuple = {:ok, :fail}
-status_tuple_with_canceled = Tuple.put_elem(status_tuple, 2, :canceled)
-IO.inspect "Combined tuple: #{status_tuple_with_canceled}"
+
+# concat_function = fn list1, list2 -> list1 ++ list2 end
+# list1 = [1, 2, 3]
+# list2 = [4, 5, 6]
+# result_concat = concat_function.(list1, list2)
+# IO.inspect "Concatenated list: #{result_concat}"
+# status_tuple = {:ok, :fail}
+# status_tuple_with_canceled = Tuple.put_elem(status_tuple, 2, :canceled)
+# IO.inspect "Combined tuple: #{status_tuple_with_canceled}"
