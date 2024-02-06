2dacb6733ee342967ff43a12205c69eac0c52b13
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-02-06 22:56:54 +0200

    update assignment3

--- tutorial_Jamk/assignment_1/lib/assignment_3/assignment3.exs
+++ tutorial_Jamk/assignment_1/lib/assignment_3/assignment3.exs
@@ -1,3 +1,4 @@
+IO.puts("----- Part 1 -----")
 IO.puts("Insert the value: ")
 number = String.trim(IO.gets(""))
 
@@ -23,3 +24,12 @@ defmodule Divisible do
 end
 
 Divisible.divisible_number(String.to_integer(number))
+
+IO.puts("----- Part 2 -----")
+annonymous = fn first,second when is_binary(first) and is_binary(second) -> "#{first}#{second}"
+first ,second -> first+second end
+result_string = annonymous.("firstname ","lastname")
+result_sum = annonymous.(5,10)
+
+IO.puts("Test with String : #{result_string}")
+IO.puts("Test with Summation : #{result_sum}")
