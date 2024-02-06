1d8bf5b1a9c722ea990cdb659f56381c57578bb9
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-02-06 22:56:35 +0200

    update assignment3_2

--- tutorial_Jamk/assignment_1/lib/assignment_3/assignment3_2.exs
+++ tutorial_Jamk/assignment_1/lib/assignment_3/assignment3_2.exs
@@ -1,3 +1,8 @@
-#annonymous function with two parameters
-annonymous = fn (a,b) -> a+b end
-IO.puts(annonymous.(2,3))
+IO.puts("----- Part 2 -----")
+annonymous = fn first,second when is_binary(first) and is_binary(second) -> "#{first}#{second}"
+first ,second -> first+second end
+result_string = annonymous.("firstname ","lastname")
+result_sum = annonymous.(5,10)
+
+IO.puts("Test with String : #{result_string}")
+IO.puts("Test with Summation : #{result_sum}")
