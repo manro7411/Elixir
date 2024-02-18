2de5390d002819cdde54ab1c7fa66697022e242d
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-02-18 12:14:40 +0200

    update part3 to git

--- tutorial_Jamk/assignment_1/lib/assignment_1/part3.exs
+++ tutorial_Jamk/assignment_1/lib/assignment_1/part3.exs
@@ -1,7 +1,12 @@
-user = IO.gets("Enter the Sentences : ")
-alphabet =  String.length(String.trim(user))
-IO.puts("Sentences : #{alphabet}");
-reverse_alphabet = String.reverse(alphabet);
-IO.puts("Reverse Sentence #{reverse_alphabet}")
-modified = String.replace(user,"foo",'bar')
-IO.puts("Text is modified : {modified}")
+user = IO.gets("Enter the Sentence: ")
+trimmed_user = String.trim(user)
+alphabet_length = String.length(trimmed_user)
+IO.puts("Sentence: #{trimmed_user}")
+
+# Reverse the sentence
+reverse_sentence = String.reverse(trimmed_user)
+IO.puts("Reverse Sentence: #{reverse_sentence}")
+
+# Modify the text
+modified_sentence = String.replace(trimmed_user, "foo", "bar")
+IO.puts("Modified Text: #{modified_sentence}")
