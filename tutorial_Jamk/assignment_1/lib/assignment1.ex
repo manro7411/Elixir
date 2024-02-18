51a64c50e75ae070e1bd67a2f332108c0ab6f8ad
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-02-18 12:13:49 +0200

    update assignment1.ex

--- tutorial_Jamk/assignment_1/lib/assignment1.ex
+++ tutorial_Jamk/assignment_1/lib/assignment1.ex
@@ -1,3 +1,18 @@
 defmodule Assignment1 do
+  @moduledoc """
+  Documentation for `Assignment1`.
+  """
 
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Assignment1.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
 end
