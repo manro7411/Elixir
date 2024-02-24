df8f0ec61edad54f125d22e262944932d1c6b19a
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-02-24 13:15:43 +0200

    update -> math.ex

---
+++ tutorial_Jamk/assignment_1/lib/assignment_5/math.ex
@@ -0,0 +1,22 @@
+defmodule Assignment5.Math do
+  def addition(a,b) do
+    IO.puts "The value a is #{a} and b is #{b}"
+    a+b
+  end
+
+  def subtraction(a,b) do
+    IO.puts "The value a is #{a} and b is #{b}"
+    a-b
+  end
+
+  def multiplication(a,b) do
+    IO.puts "The value a is #{a} and b is #{b}"
+    a*b
+  end
+
+  def division(a,b) do
+    IO.puts "The value a is #{a} and b is #{b}"
+    a/b
+  end
+
+end
