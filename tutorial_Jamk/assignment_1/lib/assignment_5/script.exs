b56b05ce15428230b82b99ce18d14599e12fa9c5
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-02-25 10:46:20 +0200

    update -> changed somthing confusing

--- tutorial_Jamk/assignment_1/lib/assignment_5/script.exs
+++ tutorial_Jamk/assignment_1/lib/assignment_5/script.exs
@@ -1,16 +1,20 @@
-defmodule Main do
+defmodule MyMain do
   def start do
     loop()
   end
+
   def loop do
     IO.puts("Enter the values : ")
-    input = IO.gets(:stdio,"") |> String.trim()
-    case  Calculator.cal(input) do
-      :error -> IO.puts("Exiting")
+    input = IO.gets(:stdio, "") |> String.trim()
+
+    case Assignment5.Calculator.calc(input) do
+      :error ->
+        IO.puts("Exiting...")
       result ->
         IO.puts("Result: #{result}")
         loop()
     end
   end
 end
-Main.start()
+
+MyMain.start()
