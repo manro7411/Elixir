d278dc35d784032aeec92aa2571c4008efeff68c
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-03-09 14:01:01 +0200

    Update -> def demoter(user)

--- tutorial_Jamk/assignment_1/lib/assignment_6/assignment6.exs
+++ tutorial_Jamk/assignment_1/lib/assignment_6/assignment6.exs
@@ -1,9 +1,33 @@
-defmodule User do
-  defstruct firstname: "", lastname: "", id_number: nil, salary: 0, job: :none
-  def new_user(attrs) do
-    %User{attrs | id_number: get_next_id()}
+defmodule Employee do
+  @counter 0
+
+  defstruct first_name: "", last_name: "", id_number: nil, salary: 0, job: :none
+
+  def new_employee(attrs) do
+    %Employee{attrs | id_number: get_next_id()}
   end
+
   defp get_next_id do
-    id_number + 1
+    id = @counter + 1
+    @counter = id
+    id
+  end
+
+  def promote(employee) do
+    %Employee{employee | job: next_job(employee.job), salary: next_salary(employee.salary)}
+  end
+
+  def demote(employee) do
+    %Employee{employee | job: :none, salary: 0}
+  end
+
+  defp next_job(:none), do: :coder
+  defp next_job(:coder), do: :designer
+  defp next_job(:designer), do: :manager
+  defp next_job(:manager), do: :ceo
+  defp next_job(:ceo), do: :ceo
+
+  defp next_salary(salary) do
+    salary + 2000
   end
 end
