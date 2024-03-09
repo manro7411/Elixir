f52008d8f924413c225593d39aec31d6376b07ee
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-03-09 12:41:33 +0200

    Update -> reminder to set the default value of salary and job to 0 and none

--- tutorial_Jamk/assignment_1/lib/assignment_6/assignment6.exs
+++ tutorial_Jamk/assignment_1/lib/assignment_6/assignment6.exs
@@ -1,11 +1,14 @@
 defmodule User do
   defstruct firstname: "", lastname: "",id_number: nil,salary: 0,job: none;
+  # salary has a default value 0
+  # job has a default value of :none
 
   def new_User(inp) do
 
   end
 
   defp get_id do
+    id_number+1
 
   end
 
