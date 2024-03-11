3b710deb6cb6dde29fb6a5d5786d2ab943905fe9
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-03-11 13:53:36 +0200

    Update -> def designer and manager

--- tutorial_Jamk/assignment_1/lib/assignment_6/employee.ex
+++ tutorial_Jamk/assignment_1/lib/assignment_6/employee.ex
@@ -7,14 +7,14 @@ defmodule Employee do
 
   def promote(%Employee{job: :none} = employee), do: %{employee | job: :coder, salary: 20000.00}
   def promote(%Employee{job: :coder} = employee), do: %{employee | job: :designer, salary: 4000.0}
-  # def promote(%Employee{job: :designer} = employee), do: %{employee | job: :manager, salary: 6000.0}
-  # def promote(%Employee{job: :manager} = employee), do: %{employee | job: :ceo, salary: 8000.0}
+  def promote(%Employee{job: :designer} = employee), do: %{employee | job: :manager, salary: 6000.0}
+  def promote(%Employee{job: :manager} = employee), do: %{employee | job: :ceo, salary: 8000.0}
   def promote(employee), do: employee  # No change for :ceo and others
 
   def demote(%Employee{job: :none} = employee), do: %{employee | job: :none, salary: 0.0}
   def demote(%Employee{job: :coder} = employee), do: %{employee | job: :none, salary: 0.0}
   def demote(%Employee{job: :designer} = employee), do: %{employee | job: :coder, salary: 2000.0}
-  # def demote(%Employee{job: :manager} = employee), do: %{employee | job: :designer, salary: 4000.0}
-  # def demote(%Employee{job: :ceo} = employee), do: %{employee | job: :manager, salary: 6000.0}
+  def demote(%Employee{job: :manager} = employee), do: %{employee | job: :designer, salary: 4000.0}
+  def demote(%Employee{job: :ceo} = employee), do: %{employee | job: :manager, salary: 6000.0}
   def demote(employee), do: employee  # No change for :none and others
 end
